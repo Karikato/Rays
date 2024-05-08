e1f9e54fa36b014797d162bd6187907ba4cf1d8a
Karikato <21bce227@nirmauni.ac.in>
2024-05-08 17:35:06 +0530

    yes

--- Javascript/script.js
+++ Javascript/script.js
@@ -34,11 +34,3 @@ document.addEventListener("DOMContentLoaded", function() {
     });
 });
 
-document.addEventListener('DOMContentLoaded', function() {
-    const menuToggle = document.getElementById('menu-toggle');
-    const nav = document.querySelector('nav');
-
-    menuToggle.addEventListener('click', function() {
-        nav.classList.toggle('show-nav');
-    });
-});
